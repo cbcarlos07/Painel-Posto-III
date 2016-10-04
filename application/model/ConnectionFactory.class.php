e51440d6a2fb45a8c2832cd730eec3b40724acc4
cbcarlos07 <cbcarlos08@gmail.com>
2016-10-04 10:52:23 -0400

    login
    senha

--- application/model/ConnectionFactory.class.php
+++ application/model/ConnectionFactory.class.php
@@ -1,13 +1,13 @@
 <?php   
 class ConnectionFactory{
-    private $ora_user = "dbaadv"; 
-    private $ora_senha = "dbaadv"; 
+    private $ora_user = "login"; 
+    private $ora_senha = "senha"; 
     private $ora_bd = "(DESCRIPTION=
                         (ADDRESS_LIST=
-                        (ADDRESS=(PROTOCOL=TCP)(HOST=10.51.26.60)(PORT=1521))
+                        (ADDRESS=(PROTOCOL=TCP)(HOST=ip)(PORT=1521))
                         )
                         (CONNECT_DATA=
-                        (SERVICE_NAME=prdmv)
+                        (SERVICE_NAME=servico)
                         )
                         )"; 
     public  function  getConnection(){
